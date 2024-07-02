159c8b9c17aae111a3711fc10800502a2b4f129f
Ojk <Ojk@OJKSERVER>
2024-07-02 09:18:06 +0800

    更新

--- IdaPython/main.py
+++ IdaPython/main.py
@@ -7,9 +7,6 @@ b = jfx_bridge_ida.IDABridge()
 idaapi = b.get_idaapi()
 idc = b.get_idc()
 idautils = b.get_idautils()
-
-print(idc.ScreenEA())
-
-# or use the sark module for easier scripting! Remember to install sark in the IDA python environment
-sark = b.get_sark()
-print(sark.Line())
+ea=0x1028BADE0
+print(idc.here())
+print(idc.SegName(ea))
